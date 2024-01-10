81e5766a6e2ab5b752467dd25d05d518799270d4
Bam <khanguyen1204@gmail.com>
2024-01-10 15:01:22 +0700

    feat: add API userGetRate

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -17,4 +17,18 @@ const userGetLike = async (req, res) => {
   }
 };
 
-export { userGetLike };
+const userGetRate = async (req, res) => {
+  try {
+    let { userId } = req.params;
+    let data = await conn.rate_res.findAll({
+      where: {
+        user_id: userId,
+      },
+    });
+    res.send(data);
+  } catch (e) {
+    res.send(`userGetRate error ${e}`);
+  }
+};
+
+export { userGetLike, userGetRate };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { userGetLike } from "../controllers/userControllers.js";
+import { userGetLike, userGetRate } from "../controllers/userControllers.js";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/user-get-like-onId/:userId", userGetLike);
+userRoutes.get("/user-get-rate-onId/:userId", userGetRate);
 export default userRoutes;
